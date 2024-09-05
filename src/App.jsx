2462989301fa32d14a9ea462a839a99dc7c496ae
Rohit Pandey <rd8614196@gmail.com>
2024-09-05 14:53:01 +0530

    git init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Login from './pages/Login/Login'
+// import { Outlet } from 'react-router-dom'
+
+const App = () => {
+  // const [isLoading, setIsLoading] = useState(true);
+  // return (
+  //   <div>App</div>
+  // )
+
+  return <>
+    <Login/>
+  </>
+}
+
+export default App
